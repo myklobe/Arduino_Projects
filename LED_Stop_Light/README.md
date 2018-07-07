526635ab6b3a4d8d5884961b65291850196d24b7
Michael Wiggin <myklobe@gmail.com>
2018-07-07 01:17:37 -0500

    Renaming LED Stop Light

---
+++ LED_Stop_Light/README.md
@@ -0,0 +1,21 @@
+"# Arduino_Projects" 
+
+For use with Arduino Create Studio - https://create.arduino.cc/editor/
+
+I have created a simple Stop Light LED blinking sketch to work with an Arduino Mega
+
+My LED Matrix: https://www.amazon.com/gp/product/B06W9F1J2Z/ - 
+Cylewet MAX7219 Dot Matrix Module 4 in 1 Display for Arduino Microcontroller with 5Pin Line CYT1045
+
+My Arduino Mega - https://www.amazon.com/gp/product/B016JWNYBE
+
+My Setup:
+I connected a jumper wire from the ground pin on Arduino to ground bank on my breadboard
+From ground bank on breadboard, I jumped a 330 ohm (up to 1k) resistor to row 6 on my breadboard
+I plugged in the short lead on the Green LED to row 6 on the breadboard
+I plugged in the long lead on the Green LED in to row 5 on breadboard
+I connected a red wire from row 5 on the Breadboard to Pin 8 on Arduino
+
+I repeated the previous steps with a Yellow LED on Pin 7 and a Red LED on pin 8.
+
+My code uses these values. You can modify them for whichever pins you wish on your Arduino.
---
+++ LED_Stop_Light/stop_light.ino
@@ -0,0 +1,37 @@
+// Basic blinking LED light using pin 8
+// Connect a wire from the ground pin on Arduino to ground bank on breadboard
+// From ground bank on breadboard, jump a 330 ohm resistor to row 6 on breadboard
+// Plug in short lead on LED to row 6 on breadboard
+// Plug in long lead on LED on row 5 on breadboard
+// Connect Red wire from row 5 on the Arduino to Pin 8 on Arduino
+// Repeat on pins 7 and 6 for yellow and green LEDs
+// You can reuse the ground jumper
+
+int redLEDPin8 = 8;                // LED connected to pin 8
+int yellowLEDPin7 = 7;
+int greenLEDPin6 = 6;
+void setup()
+{
+  pinMode(redLEDPin8, OUTPUT);      // sets the digital pin as output
+  pinMode(yellowLEDPin7, OUTPUT);      // sets the digital pin as output
+  pinMode(greenLEDPin6, OUTPUT);      // sets the digital pin as output
+}
+
+void loop()
+{
+  digitalWrite(redLEDPin8, HIGH);   // sets the LED on
+  delay(5000);                  // waits for half a second
+  digitalWrite(redLEDPin8, LOW);    // sets the LED off
+  delay(50);                  // waits for half a second
+  
+  digitalWrite(yellowLEDPin7, HIGH);   // sets the LED on
+  delay(2000);                  // waits for half a second
+  digitalWrite(yellowLEDPin7, LOW);    // sets the LED off
+  delay(50);                  // waits for half a second
+  
+  digitalWrite(greenLEDPin6, HIGH);   // sets the LED on
+  delay(5000);                  // waits for half a second
+  digitalWrite(greenLEDPin6, LOW);    // sets the LED off
+  delay(50);                  // waits for half a second
+  
+}
