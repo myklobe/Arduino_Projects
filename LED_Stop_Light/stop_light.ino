de1a18cd366293770949ef860771899b754c9a62
Michael Wiggin <myklobe@gmail.com>
2018-07-07 01:23:39 -0500

    updated Sketch

--- LED_Stop_Light/stop_light.ino
+++ LED_Stop_Light/stop_light.ino
@@ -20,18 +20,18 @@ void setup()
 void loop()
 {
   digitalWrite(redLEDPin8, HIGH);   // sets the LED on
-  delay(5000);                  // waits for half a second
+  delay(5000);                  // waits for 5 seconds
   digitalWrite(redLEDPin8, LOW);    // sets the LED off
-  delay(50);                  // waits for half a second
+  delay(50);                  // waits for 1/20 a second
   
   digitalWrite(yellowLEDPin7, HIGH);   // sets the LED on
-  delay(2000);                  // waits for half a second
+  delay(2000);                  // waits for 2 seconds
   digitalWrite(yellowLEDPin7, LOW);    // sets the LED off
-  delay(50);                  // waits for half a second
+  delay(50);                  // waits for 1/20 second
   
   digitalWrite(greenLEDPin6, HIGH);   // sets the LED on
-  delay(5000);                  // waits for half a second
+  delay(5000);                  // waits for 5 seconds
   digitalWrite(greenLEDPin6, LOW);    // sets the LED off
-  delay(50);                  // waits for half a second
+  delay(50);                  // waits for 1/20 second
   
 }
