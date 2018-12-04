1e1339cd6b1ed23ddb8e7a02f3abf5f901461530
Michael Wiggin <mwiggin@alertlogic.com>
2018-12-04 16:43:46 +0000

    Updated Documentation

--- LED_Stop_Light/stop_light.ino
+++ LED_Stop_Light/stop_light.ino
@@ -1,4 +1,4 @@
-// Basic blinking LED light using pin 8
+// Basic blinking LED lights using pins 6, 7, and 8
 // Connect a wire from the ground pin on Arduino to ground bank on breadboard
 // From ground bank on breadboard, jump a 330 ohm resistor to row 6 on breadboard
 // Plug in short lead on LED to row 6 on breadboard
@@ -7,31 +7,32 @@
 // Repeat on pins 7 and 6 for yellow and green LEDs
 // You can reuse the ground jumper
 
-int redLEDPin8 = 8;                // LED connected to pin 8
-int yellowLEDPin7 = 7;
-int greenLEDPin6 = 6;
+int redLEDPin8 = 8;                   // Red LED connected to pin 8
+int yellowLEDPin7 = 7;                // Yellow LED connected to pin 7 
+int greenLEDPin6 = 6;                 // Green LED connected to pin 6
+
 void setup()
 {
-  pinMode(redLEDPin8, OUTPUT);      // sets the digital pin as output
-  pinMode(yellowLEDPin7, OUTPUT);      // sets the digital pin as output
+  pinMode(redLEDPin8, OUTPUT);        // sets the digital pin as output
+  pinMode(yellowLEDPin7, OUTPUT);     // sets the digital pin as output
   pinMode(greenLEDPin6, OUTPUT);      // sets the digital pin as output
 }
 
 void loop()
 {
-  digitalWrite(redLEDPin8, HIGH);   // sets the LED on
-  delay(5000);                  // waits for 5 seconds
-  digitalWrite(redLEDPin8, LOW);    // sets the LED off
-  delay(50);                  // waits for 1/20 a second
+  digitalWrite(redLEDPin8, HIGH);     // sets the Red LED on
+  delay(5000);                        // waits for 5 seconds
+  digitalWrite(redLEDPin8, LOW);      // sets the Red LED off
+  delay(50);                          // waits for 1/20 second
   
-  digitalWrite(yellowLEDPin7, HIGH);   // sets the LED on
-  delay(2000);                  // waits for 2 seconds
-  digitalWrite(yellowLEDPin7, LOW);    // sets the LED off
-  delay(50);                  // waits for 1/20 second
+  digitalWrite(yellowLEDPin7, HIGH);  // sets the Yellow LED on
+  delay(2000);                        // waits for 2 seconds
+  digitalWrite(yellowLEDPin7, LOW);   // sets the Yellow LED off
+  delay(50);                          // waits for 1/20 second
   
-  digitalWrite(greenLEDPin6, HIGH);   // sets the LED on
-  delay(5000);                  // waits for 5 seconds
-  digitalWrite(greenLEDPin6, LOW);    // sets the LED off
-  delay(50);                  // waits for 1/20 second
+  digitalWrite(greenLEDPin6, HIGH);   // sets the Green LED on
+  delay(5000);                        // waits for 5 seconds
+  digitalWrite(greenLEDPin6, LOW);    // sets the Green LED off
+  delay(50);                          // waits for 1/20 second
   
 }
